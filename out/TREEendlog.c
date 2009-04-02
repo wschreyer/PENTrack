1bbf0b3029be9e2cfeb9719b16ce691ec2ea1d11
marco <marco@63a68f78-dbc9-4259-be3e-50ca72a5ff40>
2009-04-02 07:36:44 +0000

    - TREEDRAWtrack.c and TREEendlog.c updated

--- out/TREEendlog.c
+++ out/TREEendlog.c
@@ -1,12 +1,13 @@
 //
-//	TREEendlog.c is a ROOT scipt that reads and edits a data files first line to refine the branch descriptor in order to
-//	construct a ROOT tree named "[filename].root" and finally  restores the first line to its original. For each variable
-//	will a branch be created in the tree contianing the data as double type by default.
+//	TREEendlog.c is a ROOT (5.23/01) scipt that reads and edits a data files first line to refine the branch descriptor
+//	in order to construct a ROOT tree named "[filename].root" and finally  restores the first line to its original. For
+//	each variable will a branch be created in the tree contianing the data as double type by default.
 //
 //	User Instructions:
 //	(1) Make sure the data files first line contains the names of the variables seperated by '[SPACE]' (e.g. "x y z")!
 //	(2) Make sure that the data in the following lines are seperated by '[SPACE]', too (e.g. "0.42 0.42 0.42")!
-//	(3) Run the script in ROOT like this: root [0] .x TREEendlog.c("[filename]");
+//	(3) Make sure that the data are compatible to the type D / Double_t (a 64 bit floating point)!
+//	(4) Run the script in ROOT like this: root [0] .x TREEendlog.c("[filename]");
 //
 //	Note:
 //	(1) Any occurrence of errors may possibly lead to a loss of data.
