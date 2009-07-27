9b9349e104576855dd4b6355c2e842bb7ea5e8f7
marco <marco@63a68f78-dbc9-4259-be3e-50ca72a5ff40>
2009-07-27 12:04:17 +0000

    - new MCZerfallsstartwerte() in mc.c and subfunctions for the decay simulation - few minor changes for decay simulation in bahnen2005.c and main.c - 'decay.error' in end.log included

--- mc.h
+++ mc.h
@@ -1,1 +1,5 @@
 extern void MCStartwerte(long double delx);
+extern void MCZerfallsstartwerte();
+extern long double GetAngle(long double cos, long double sin);
+extern void zBOOST(long double BOOSTbeta, long double* Dp0, long double* Dp3);
+extern void zyROTROT(long double zROTbeta, long double yROTdelta, long double* Dp1, long double* Dp2, long double* Dp3);
