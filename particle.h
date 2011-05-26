04471b7916ac7c66157e170f52ee0d8ab86fd120
Wolfgang Schreyer <w.schreyer@tum.de>
2011-05-26 12:46:18 +0000

    - fixed adiabacity checks

--- particle.h
+++ particle.h
@@ -587,7 +587,7 @@ protected:
 		bool result = false;
 		long double B[4][4];
 		field->BFeld(y1[0],y1[1],y1[2],x1,B);
-		if (B[3][0] < 0){
+		if (B[3][0] > 0){
 			// spin flip properties according to Vladimirsky and thumbrule
 			vlad = vladimirsky(B[0][0], B[1][0], B[2][0],
 							   B[0][1], B[0][2], B[0][3], B[1][1], B[1][2], B[1][3], B[2][1], B[2][2], B[2][3], B[3][0],
