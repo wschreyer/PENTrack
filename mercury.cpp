b5b70275f656437b433fb0cec7d93012e6843cd2
ssc5 <schah063@uottawa.ca>
2015-11-13 16:45:36 -0800

    Added co-magnetometers to cmake list and defined reflect method

--- mercury.cpp
+++ mercury.cpp
@@ -6,7 +6,7 @@
 #include "globals.h"
 #include "mercury.h"
 
-const char* NAME_MERCURY = "mercury";
+const char* NAME_MERCURY = "mercury-199";
 
 ofstream TMercury::endout; ///< endlog file stream
 ofstream TMercury::snapshotout; ///< snapshot file stream
@@ -23,23 +23,64 @@ TMercury::TMercury(int number, double t, double x, double y, double z, double E,
 
 void TMercury::OnHit(value_type x1, state_type y1, value_type &x2, state_type &y2, int &polarisation,
 			const double normal[3], solid *leaving, solid *entering, bool &trajectoryaltered, bool &traversed){
-	traversed = true;
 	trajectoryaltered = false;
+	traversed = true;
+	
+	Reflect(x1, y1, x2, y2, polarisation, normal, leaving, entering, trajectoryaltered, traversed);
 }
 
 
-bool TMercury::OnStep(value_type x1, state_type y1, value_type &x2, state_type &y2, int &polarisation, solid currentsolid){
-	if (currentsolid.ID != geom->defaultsolid.ID){
+void TMercury::Reflect(value_type x1, state_type y1, value_type &x2, state_type &y2, int &polarisation,
+			const double normal[3], solid *leaving, solid *entering, bool &trajectoryaltered, bool &traversed){
+	
+	value_type vnormal = y1[3]*normal[0] + y1[4]*normal[1] + y1[5]*normal[2]; // velocity normal to reflection plane
+	//particle was neither transmitted nor absorbed, so it has to be reflected
+	double prob = mc->UniformDist(0,1);
+	material *mat = vnormal < 0 ? &entering->mat : &leaving->mat;
+	double diffprob = mat->DiffProb;
+//	cout << "prob: " << diffprob << '\n';
+	
+	if (prob >= diffprob){
+		//************** specular reflection **************
+//				printf("Specular reflection! Erefl=%LG neV\n",Enormal*1e9);
 		x2 = x1;
 		for (int i = 0; i < 6; i++)
 			y2[i] = y1[i];
-		StopIntegration(ID_ABSORBED_IN_MATERIAL, x2, y2, polarisation, currentsolid);
-		printf("Absorption!\n");
-		return true;
+		y2[3] -= 2*vnormal*normal[0]; // reflect velocity
+		y2[4] -= 2*vnormal*normal[1];
+		y2[5] -= 2*vnormal*normal[2];
+	}
+	else{
+		//************** diffuse reflection no MR model ************
+		double phi_r, theta_r;	
+		
+		phi_r = mc->UniformDist(0, 2*pi); // generate random reflection angles (Lambert's law)
+		theta_r = mc->SinCosDist(0, 0.5*pi);
+		
+		if (vnormal > 0) theta_r = pi - theta_r; // if velocity points out of volume invert polar angle
+		x2 = x1;
+		for (int i = 0; i < 3; i++)
+			y2[i] = y1[i];
+		value_type vabs = sqrt(y1[3]*y1[3] + y1[4]*y1[4] + y1[5]*y1[5]);
+		y2[3] = vabs*cos(phi_r)*sin(theta_r);	// new velocity with respect to z-axis
+		y2[4] = vabs*sin(phi_r)*sin(theta_r);
+		y2[5] = vabs*cos(theta_r);
+		RotateVector(&y2[3], normal, &y1[3]); // rotate velocity into coordinate system defined by incoming velocity and plane normal
+//				printf("Diffuse reflection! Erefl=%LG neV w_e=%LG w_s=%LG\n",Enormal*1e9,phi_r/conv,theta_r/conv);
 	}
 
-	return false;
-}
+	if (mc->UniformDist(0,1) < entering->mat.SpinflipProb){
+		polarisation *= -1;
+		Nspinflip++;
+	}
+
+	traversed = false;
+	trajectoryaltered = true;
+} // end reflect method
+
+//do nothing for each for step
+bool TMercury::OnStep(value_type x1, state_type y1, value_type &x2, state_type &y2, int &polarisation, solid currentsolid) { return false; }
 
 //Mercury does not decay
 void TMercury::Decay(){}
+
--- mercury.h
+++ mercury.h
@@ -11,14 +11,14 @@
 extern const char* NAME_MERCURY; ///< name of TMERCURY class
 
 /**
- * Mercury particle class.
+ * Mercury-199 particle class.
  *
  * Simulates a mercury atom including gravitation and Lorentz-force
  */
 struct TMercury: TParticle{
 public:
 	/**
-	 * Create mercury atom.
+	 * Create mercury-199 atom.
 	 *
 	 * Wraps basic TParticle constructor
 	 *
@@ -45,9 +45,8 @@ protected:
 	static ofstream spinout; ///< spinlog file stream
 
 	/**
-	 * This method is executed, when a particle crosses a material boundary.
-	 *
-	 * Currently, nothing happens to mercury atoms, but this will be changed after some research on their behaviour is done.
+	 * This method is executed, when a particle encounters a material boundary.
+	 * The atom is reflected specularly or diffusely depending on the material type. 
 	 *
 	 * @param x1 Start time of line segment
 	 * @param y1 Start point of line segment
@@ -63,12 +62,18 @@ protected:
 	void OnHit(value_type x1, state_type y1, value_type &x2, state_type &y2, int &polarisation,
 				const double normal[3], solid *leaving, solid *entering, bool &trajectoryaltered, bool &traversed);
 
-
+	/**
+	 * Reflect mercury-199 from surface.
+	 *
+	 * Reflects or scatters the neutron according to specular or Lambert.
+	 */
+	void Reflect(value_type x1, state_type y1, value_type &x2, state_type &y2, int &polarisation,
+				const double normal[3], solid *leaving, solid *entering, bool &trajectoryaltered, bool &traversed);
+	
 	/**
 	 * This method is executed on each step.
 	 *
-	 * Mercury atoms are immediately absorbed in solids other than TParticle::geom::defaultsolid, this behaviour will be changed after
-	 * some research is done. 
+	 * Do nothing. 
 	 *
 	 * @param x1 Start time of line segment
 	 * @param y1 Start point of line segment
