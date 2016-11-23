ee8ccf4e087f7d8c3f9fb54e7334bfb9f3fc657f
wschreyer <w.schreyer@tum.de>
2016-11-23 11:42:04 +0100

    Download latest CGAL release instead of Github repo Takes way less disk space.

--- install_cgal.sh
+++ install_cgal.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-echo "I'm going to download and compile CGAL for you. This will take up to 1GB of disk space. Continue?"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) break;;
-        No ) exit;;
-    esac
-done
+echo "I'm going to download and compile CGAL for you.\nThis will take about 100MB of disk space.\nPress ENTER to continue, Ctrl+C to cancel."
+read -p "" key
 
-git submodule init
-git submodule update
+mkdir cgal/
+cd cgal/
+wget -nc https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.9/CGAL-4.9.tar.xz
+tar -xf CGAL-4.9.tar.xz
+rm CGAL-4.9.tar.xz
+cd ..
+
 mkdir cgal/build
 cd cgal/build
-cmake ../src
-make -j8
+cmake ../CGAL-4.9/
+make
 cd ../..
