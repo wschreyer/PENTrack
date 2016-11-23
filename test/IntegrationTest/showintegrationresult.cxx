c471fb2bd38ce785d8f8b42d9a47d5ad744a7575
Wolfgang Schreyer <w.schreyer@tum.de>
2016-11-23 21:34:52 +0100

    Fixed ROOT scripts of test simulations again.

--- test/IntegrationTest/showintegrationresult.cxx
+++ test/IntegrationTest/showintegrationresult.cxx
@@ -1,7 +1,7 @@
-int showintegrationresult(){
+void showintegrationresult(){
 	// a = -g - mu*dBdz/m
 	// z(t) = z_start + v_z_start*t + a/2*t^2
 	// t(z=zend) = (-v_z_start +- sqrt(v_z_start^2 - 2*a*(z_start - z_end)))/a
 	// x_hit = x_start + v_x_start*t(z=0)
-	return neutronend->Draw("xend - xstart - vxstart*(-vzstart - sqrt(vzstart*vzstart - 2*-15.5754762729*(zstart - zend)))/-15.5754762729");
+	neutronend->Draw("xend - xstart - vxstart*(-vzstart - sqrt(vzstart*vzstart - 2*-15.5754762729*(zstart - zend)))/-15.5754762729");
 }
