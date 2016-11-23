cff4a89123da6b18f31f4aef4cf4ea682ec06f91
Wolfgang Schreyer <w.schreyer@tum.de>
2016-11-23 21:12:36 +0100

    Fixed ROOT scripts of test simulations

--- test/2DFieldTest/showfieldmap.cxx
+++ test/2DFieldTest/showfieldmap.cxx
@@ -1,3 +1,3 @@
-int showfieldmap(){
+void showfieldmap(){
 	mytree->Draw("sqrt(Bx*Bx+By*By+Bz*Bz) - abs(cos(sqrt(x*x+y*y)*pi) + cos(z*pi)):sqrt(x*x+y*y):z","","");
 }
--- test/IntegrationTest/showintegrationresult.cxx
+++ test/IntegrationTest/showintegrationresult.cxx
@@ -3,5 +3,5 @@ int showintegrationresult(){
 	// z(t) = z_start + v_z_start*t + a/2*t^2
 	// t(z=zend) = (-v_z_start +- sqrt(v_z_start^2 - 2*a*(z_start - z_end)))/a
 	// x_hit = x_start + v_x_start*t(z=0)
-	neutronend->Draw("xend - xstart - vxstart*(-vzstart - sqrt(vzstart*vzstart - 2*-15.5754762729*(zstart - zend)))/-15.5754762729");
+	return neutronend->Draw("xend - xstart - vxstart*(-vzstart - sqrt(vzstart*vzstart - 2*-15.5754762729*(zstart - zend)))/-15.5754762729");
 }
