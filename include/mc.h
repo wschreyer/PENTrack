409f86a1a2defdb63e7dc8a974406d5b2c7146a0
wschreyer <w.schreyer@tum.de>
2016-12-23 16:45:44 +0100

    Updated documentation of random-number generators

--- include/mc.h
+++ include/mc.h
@@ -8,63 +8,79 @@
 
 #include <random>
 
-/**
- * Class to generate random numbers in several distributions.
- */
-typedef std::mt19937_64 TMCGenerator;
+typedef std::mt19937_64 TMCGenerator; ///< typedef to default random-number generator
 
 namespace std{
 
+/**
+ * Generate polarization +1 or -1 weighted by the projection of the spin-vector onto an axis.
+ *
+ * Satisfies the concept RandomNumberDistribution of STL
+ */
 template<typename T>
 class polarization_distribution{
 public:
-	typedef T result_type;
-	typedef T param_type;
+	typedef T result_type; ///< type returned by operator()
+	typedef T param_type; ///< type of parameter (projection of spin onto axis)
 private:
-	param_type _p;
+	param_type _p; ///< member containing parameter
 public:
-	polarization_distribution(){ reset(); }
-	polarization_distribution(const param_type &p){ param(p); }
-	void reset(){ _p = static_cast<T>(0.5); }
-	param_type param() const { return _p; }
-	void param(const param_type &p){ _p = p; }
+	polarization_distribution(){ reset(); } ///< empty constructor, calls reset()
+	polarization_distribution(const param_type &p){ param(p); } ///< construct with specific projection parameter
+	void reset(){ _p = static_cast<param_type>(0); } ///< reset to default state (projection == 0, +1 and -1 equally likely)
+	param_type param() const { return _p; } ///< returns stored parameter
+	void param(const param_type &p){ _p = p; } ///< set stored parameter
+
+	/**
+	 * Return polarization +1/-1 weighted by projection parameter p
+	 */
 	template<class Random> result_type operator()(Random &r, const param_type &p) const{
-		std::bernoulli_distribution d(p);
-		return d(r) ? 1 : -1;
+		std::bernoulli_distribution d(static_cast<param_type>(0.5)*(p + static_cast<param_type>(1)));
+		return d(r) ? static_cast<result_type>(1) : static_cast<result_type>(-1); // return +1/-1 according to Bernoulli distribution with parameter 0.5*(p + 1)
 	}
-	template<class Random> result_type operator()(Random &r) const { return operator()(r, _p); }
-	result_type min() const { return static_cast<T>(-1); }
-	result_type max() const { return static_cast<T>(1); }
-	bool operator==(const polarization_distribution<T> &rhs) const { return _p == rhs._p; }
-	bool operator!=(const polarization_distribution<T> &rhs) const { return !(operator==(rhs)); }
+	template<class Random> result_type operator()(Random &r) const { return operator()(r, _p); } ///< Return polarization +1/-1 weighted by internally stored projection parameter
+	result_type min() const { return static_cast<result_type>(-1); } ///< return min random value (-1)
+	result_type max() const { return static_cast<result_type>(1); } ///< return max random value (+1)
+	bool operator==(const polarization_distribution<T> &rhs) const { return _p == rhs._p; } ///< equality operator (compares internal parameters)
+	bool operator!=(const polarization_distribution<T> &rhs) const { return !(operator==(rhs)); } ///< inequality operator (compares internal parameters)
 };
 
+/**
+ * Generate random numbers between two values using inverse transform sampling.
+ *
+ * Template parameter func expects function result_type(result_type x, result_type min, result_type max) returning the inverse of the cumulative distribution function.
+ * Satisfies the concept RandomNumberDistribution of STL.
+ */
 template<typename T, typename func>
 class inversetransform_sampler{
 public:
-	typedef T result_type;
-	typedef std::pair<result_type, result_type > param_type;
+	typedef T result_type; ///< type returned by operator()
+	typedef std::pair<result_type, result_type > param_type; ///< type of parameter (pair of min and max values)
 private:
-	param_type _p;
+	param_type _p; ///< internally stored parameter
 public:
-	inversetransform_sampler(){ reset(); }
-	inversetransform_sampler(const param_type &p){ param(p); }
-	inversetransform_sampler(const T min, const T max){	param(make_pair(min, max)); }
-	void reset(){ param(make_pair(static_cast<T>(0), static_cast<T>(1))); };
-	param_type param() const { return _p; }
-	void param(const param_type &p){ _p = p; }
+	inversetransform_sampler(){ reset(); } ///< default constructor, calls reset()
+	inversetransform_sampler(const param_type &p){ param(p); } ///< construct using specific parameter
+	inversetransform_sampler(const result_type min, const result_type max){	param(make_pair(min, max)); } ///< construct using min and max values
+	void reset(){ param(make_pair(static_cast<result_type>(0), static_cast<result_type>(1))); }; ///< reset to default parameter (min = 0, max = 1)
+	param_type param() const { return _p; } ///< return internally stored parameter
+	void param(const param_type &p){ _p = p; } ///< set internally stored parameter
+	/**
+	 * Return random number with between min and max values using inverse transform sampling of func
+	 */
 	template<class Random> result_type operator()(Random &r, const param_type &p) const {
 		func f;
-		std::uniform_real_distribution<T> unidist(static_cast<T>(0), static_cast<T>(1));
-		return f(unidist(r), p.first, p.second);
+		std::uniform_real_distribution<result_type> unidist(static_cast<result_type>(0), static_cast<result_type>(1)); // generate random numbers between 0 and 1
+		return f(unidist(r), p.first, p.second); // call inverse of cumulative-distribution function with random number, min and max, creating correctly distributed numbers
 	}
-	template<class Random> result_type operator()(Random &r) const { return operator()(r, _p);	}
-	result_type min() const { return _p.first; }
-	result_type max() const { return _p.second; }
-	bool operator==(const inversetransform_sampler<T, func> &rhs) const { return _p == rhs._p; }
-	bool operator!=(const inversetransform_sampler<T, func> &rhs) const { return !(operator==(rhs)); }
+	template<class Random> result_type operator()(Random &r) const { return operator()(r, _p);	} ///< Return random number using internally stored min and max values
+	result_type min() const { return _p.first; } ///< return internally stored min value
+	result_type max() const { return _p.second; } ///< return internally stored max value
+	bool operator==(const inversetransform_sampler<result_type, func> &rhs) const { return _p == rhs._p; } ///< comparison operator, compares internal min and max values
+	bool operator!=(const inversetransform_sampler<result_type, func> &rhs) const { return !(operator==(rhs)); } ///< inequality operator, compares internal min and max values
 };
 
+/*
 template<class CharT, class Traits, typename T, typename func>
 std::basic_ostream<CharT, Traits>& operator<<(std::basic_ostream<CharT, Traits> &os, const inversetransform_sampler<T, func> &d){
 	return os << "Distribution generating random numbers between " << d.min() << " and " << d.max() << " using inverse transform sampling\n";
@@ -78,49 +94,84 @@ std::basic_istream<CharT, Traits>& operator>>(std::basic_istream<CharT, Traits> 
 		d.param(p);
 	return is;
 }
+*/
 
+/**
+ * Functor to create sine-distributed random numbers with inversetransform_sampler
+ */
 template<typename T>
 struct inversetransform_sin{
 	T operator()(const T x, const T amin, const T amax) const {
-		return std::acos(std::cos(amin) - x * (std::cos(amin) - std::cos(amax)));
+		return std::acos(std::cos(amin) - x * (std::cos(amin) - std::cos(amax))); // inverse of cumulative distribution = acos(-x)
 	}
 };
+/**
+ * Specialization of inversetransform_sampler to create sine-distributed random numbers
+ */
 template<typename T>
 using sin_distribution = inversetransform_sampler<T, inversetransform_sin<T> >;
 
+
+/**
+ * Functor to create sin*cos-distributed random numbers with inversetransform_sampler
+ */
 template<typename T>
 struct inversetransform_sincos{
 	T operator()(const T x, const T amin, const T amax) const {
-		return acos(sqrt(x*(cos(amax)*cos(amax) - cos(amin)*cos(amin)) + cos(amin)*cos(amin)));
+		return acos(sqrt(x*(cos(amax)*cos(amax) - cos(amin)*cos(amin)) + cos(amin)*cos(amin))); // inverse of cumulative distribution = acos(sqrt(x))
 	}
 };
+/**
+ * Specialization of inversetransform_sampler to create sin*cos-distributed random numbers
+ */
 template<typename T>
 using sincos_distribution = inversetransform_sampler<T, inversetransform_sincos<T> >;
 
+
+/**
+ * Functor to create x^2-distributed random numbers with inversetransform_sampler
+ */
 template<typename T>
 struct inversetransform_parabolic{
 	T operator()(const T x, const T amin, const T amax) const {
-		return pow(x*(pow(amax,3) - pow(amin,3)) + pow(amin,3),1.0/3.0);
+		return pow(x*(pow(amax,3) - pow(amin,3)) + pow(amin,3),1.0/3.0); // inverse of cumulative distribution = x^1/3
 	}
 };
+/**
+ * Specialization of inversetransform_sampler to create x^2-distributed random numbers
+ */
 template<typename T>
 using parabolic_distribution = inversetransform_sampler<T, inversetransform_parabolic<T> >;
 
+
+/**
+ * Functor to create linearly distributed random numbers with inversetransform_sampler
+ */
 template<typename T>
 struct inversetransform_linear{
 	T operator()(const T &x, const T &amin, const T &amax) const {
-		return sqrt(x*(amax*amax - amin*amin) + amin*amin);
+		return sqrt(x*(amax*amax - amin*amin) + amin*amin); // inverse of cumulative distribution = sqrt(x)
 	}
 };
+/**
+ * Specialization of inversetransform_sampler to create linearly distributed random numbers
+ */
 template<typename T>
 using linear_distribution = inversetransform_sampler<T, inversetransform_linear<T> >;
 
+
+/**
+ * Functor to create sqrt-distributed random numbers with inversetransform_sampler
+ */
 template<typename T>
 struct inversetransform_sqrt{
 	T operator()(const T x, const T amin, const T amax) const {
-		return pow((pow(amax, 1.5) - pow(amin, 1.5))*x + pow(amin, 1.5), 2.0/3.0);
+		return pow((pow(amax, 1.5) - pow(amin, 1.5))*x + pow(amin, 1.5), 2.0/3.0); // inverse of cumulative distirbution = x^2/3
 	}
 };
+/**
+ * Specialization of inversetransform_sampler to create sqrt-distributed random numbers
+ */
 template<typename T> using sqrt_distribution = inversetransform_sampler<T, inversetransform_sqrt<T> >;
 
 } // end namespace std
@@ -151,7 +202,7 @@ std::piecewise_linear_distribution<double> parse_distribution(UnaryFunction f, c
 std::piecewise_linear_distribution<double> parse_distribution(const std::string &func, const double range_min, const double range_max);
 
 
-extern std::piecewise_linear_distribution<double> proton_beta_distribution;
-extern std::piecewise_linear_distribution<double> electron_beta_distribution;
+extern std::piecewise_linear_distribution<double> proton_beta_distribution; ///< Creates random numbers according to the energy spectrum of protons from free-neutron decay
+extern std::piecewise_linear_distribution<double> electron_beta_distribution; ///< Creates random numbers according to the energy spectrum of electrons from free-neutron decay
 
 #endif /*MC_H_*/
