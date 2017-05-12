3dd2777688cd9a907d014b652a941bbfa1464e49
wschreyer <w.schreyer@tum.de>
2017-05-13 00:02:23 +0200

    Merged remaining config files.

--- test/IntegrationTest/RunTest.sh
+++ test/IntegrationTest/RunTest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ../..
-./PENTrack 0 test/IntegrationTest/ test/IntegrationTest
+./PENTrack 0 test/IntegrationTest/config.in test/IntegrationTest
 cd test/IntegrationTest
 root -l -q -c ../../out/merge_all.c
 rm 000000000000neutronend.out 000000000000neutronhit.out
