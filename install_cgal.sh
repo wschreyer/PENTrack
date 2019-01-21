0bfcce071d87e55492d7d42a4b242d904ac0d0bc
wschreyer <wschreyer@tum.de>
2019-01-20 23:54:37 -0800

    Fixed issues with cmake not finding boost Updated alglib and install_cgal script to latest versions

--- install_cgal.sh
+++ install_cgal.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CGAL_VERSION=CGAL-4.11
+CGAL_VERSION=CGAL-4.13
 
 echo "I'm going to download and compile ${CGAL_VERSION} for you."
 echo "This will take about 100MB of disk space."
@@ -8,18 +8,8 @@ echo "Press ENTER to continue, Ctrl+C to cancel."
 read -p "" key
 
 mkdir cgal/
-cd cgal/
 wget -nc -O ${CGAL_VERSION}.tar.xz https://github.com/CGAL/cgal/releases/download/releases%2F${CGAL_VERSION}/${CGAL_VERSION}.tar.xz
 tar -xf ${CGAL_VERSION}.tar.xz
 rm ${CGAL_VERSION}.tar.xz
-cd ..
-
-mkdir cgal/build
-cd cgal/build
-cmake ../${CGAL_VERSION}/ -DWITH_CGAL_Qt5=OFF -DWITH_CGAL_ImageIO=OFF -DCGAL_HEADER_ONLY=ON -DCMAKE_INSTALL_PREFIX=..
-make
-make install
-cd ..
-rm -r build
-rm -r $CGAL_VERSION
-cd ..
+mv ${CGAL_VERSION}/* cgal/
+rmdir ${CGAL_VERSION}
