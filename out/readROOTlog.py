abebecc28eff05f5cb0911fada9f2212b0c33003
Wolfgang Schreyer <wschreyer@triumf.ca>
2021-10-28 17:37:50 -0700

    Added plotting examples to readROOTlog.py

--- out/readROOTlog.py
+++ out/readROOTlog.py
@@ -1,4 +1,6 @@
-# this is an example of how to read PENTrack output with the ROOTlog option enabled
+# This script is an example of how to read PENTrack output with the ROOTlog option enabled.
+# It will print some information about the simulated particles (total number, number with errors),
+# plot some particle distributions, and print some configuration parameters
 # run with
 # python readROOTlog.py *.root
 
@@ -13,6 +15,19 @@ print('{0} neutrons simulated'.format(chain.GetEntries()))
 print('{0} neutrons hit outer boundaries'.format(chain.GetEntries('stopID == -2')))
 print('{0} neutrons encountered a serious error!'.format(chain.GetEntries('stopID == -3 || stopID == -6 || stopID == -7')))
 
+c = ROOT.TCanvas('c', 'c') # create canvas to draw on
+chain.Draw('Estart') # plot histogram of starting energies
+c.Print('spectrum.pdf') # save canvas to file
+
+chain.Draw('tend', 'Estart > 100e-9') # plot histogram of stopping times of all neutrons with energy above 100neV (will overwrite contents of canvas)
+chain.Draw('tend', 'Estart < 100e-9', 'same') # plot histogram of stopping times of all neutrons with energy below 100neV on same canvas
+c.Print('tend.pdf') # save canvas to file
+
+chain.Draw('zend:xend', 'stopID > 0') # plot spatial distribution of neutrons absorbed in a material
+c.Print('absorbed.pdf')
+
+chain.Draw('zend:xend:solidend', '', 'COLZ') # plot spatial distribution of all stopped neutrons with markers colored by part ID
+c.Print('stopped.pdf')
 
 # examples of reading config variables:
 
@@ -29,3 +44,5 @@ if geometrystr:
 sourcestr = f.Get('config/SOURCE/ActiveTime') # print time source was active for
 if sourcestr:
   print('Particle source was active for {0} seconds'.format(sourcestr.String().Data().split()[0]))
+
+
